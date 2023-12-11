edec19f4238d984c4205365f8ef34c18fb9131ac
yudistirasd <yudistira.sd2@gmail.com>
2023-12-11 04:10:33 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app.vue
@@ -0,0 +1,31 @@
+<template>
+    <Head>
+        <Title>Gallery</Title>
+        <Link rel="icon" href="/icon.png" type="image/png" />
+        <Meta name="description" content="An image gallery built with Nuxt 3" />
+    </Head>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
+</template>
+
+<script setup>
+useHead({
+    htmlAttrs: {
+        lang: "en",
+    },
+});
+</script>
+
+<style>
+.layout-enter-active,
+.layout-leave-active {
+    transition: all 350ms;
+}
+.layout-enter-from,
+.layout-leave-to {
+    filter: grayscale(0.5);
+    opacity: 0;
+    filter: blur(1rem);
+}
+</style>
